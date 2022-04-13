85b81bdb01dfb56ad3bb9b670e54eec84ed4ee32
John Morrice <john.morrice.developer@gmail.com>
2022-04-13 22:16:22 +0100

    display date and time

--- renderer.js
+++ renderer.js
@@ -4,3 +4,13 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function updateDateTimeElementContent() {
+    let myDate = new Date();
+    let timeElement = document.getElementById('time');
+    let dateElement = document.getElementById('date');
+    timeElement.innerText = myDate.toLocaleTimeString();
+    dateElement.innerText = myDate.toLocaleDateString();
+}
+
+setInterval(updateDateTimeElementContent, 250);
