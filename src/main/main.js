f1f13de1a42c74611a5302b9dbeb29f5c5c70ed7
John Morrice <john.morrice.developer@gmail.com>
2022-04-19 21:57:10 +0100

    new directory structure

--- src/main/main.js
+++ src/main/main.js
@@ -12,12 +12,12 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, '../renderer/preload.js'),
     },
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, '../html/index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
