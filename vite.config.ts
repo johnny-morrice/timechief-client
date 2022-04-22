87a7b42f20eb6f9c69919ed9d946662866a34471
John Morrice <john.morrice.developer@gmail.com>
2022-04-22 17:23:52 +0100

    include the woff fonts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     build: {
         target: 'esnext',
         polyfillDynamicImport: false,
-    },
-    assetsInclude: ["assets"]
+    }
+    // assetsInclude: ["assets"]
 });
