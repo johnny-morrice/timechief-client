1ec41852729467cbd89cec7e5cf33bdf1103d09c
John Morrice <john.morrice.developer@gmail.com>
2022-04-24 00:07:14 +0100

    toward astro page

---
+++ src/renderer/astroPage.jsx
@@ -0,0 +1,64 @@
+import { createSignal } from 'solid-js';
+import { addClockDataCallback } from './ipc';
+import { getTaskBarSignals } from './taskbarSignals';
+
+// Moon phase. 0 and 1 are 'new moon', 0.25 is 'first quarter moon', 0.5 is 'full moon' and 0.75 is 'last quarter moon'. The periods in between are called 'waxing crescent', 'waxing gibous', 'waning gibous', and 'waning crescent', respectively.
+
+class AstroPageSignals {
+  constructor() {
+      [this.sunrise, this.setSunrise] = createSignal("");
+      [this.sunset, this.setSunset] = createSignal("");
+      [this.moonrise, this.setMoonrise] = createSignal("");
+      [this.moonset, this.setMoonset] = createSignal("");
+      [this.moonphase, this.setMoonphase] = createSignal("");
+
+  }
+}
+
+function updateAstroPageSignals(signals, data) {
+    let clock = data["Clock"];
+    let location = clock["Location"];
+    let deviceSerial = clock["DeviceSerial"];
+    let latitude = clock["Latitude"];
+    let longitude = clock["Longitude"];
+    let timezone = clock["Timezone"];
+    signals.setMyIPAddress("unknown IP")
+    signals.setDeviceSerial(deviceSerial);
+    signals.setLocation(location);
+    signals.setLatitude(latitude);
+    signals.setLongitude(longitude);
+    signals.setTimezone(timezone);
+}
+
+export const AstroPage = () => {
+  let configSignals = new AstroPageSignals();
+  let taskBarSignals = getTaskBarSignals();
+
+  addClockDataCallback((data) => updateAstroPageSignals(configSignals, data));
+
+  return <div id="config-screen" style={{
+        display: `${taskBarSignals.configDisplayStyle()}` 
+        }}
+        >
+        <div class="column-flex">
+            <div class='flex-element'>
+                <div id='device-serial'>{configSignals.deviceSerial}</div>
+            </div>
+            <div class='flex-element'>
+                <div id='ip-address'>{configSignals.myIPAddress}</div>
+            </div>
+            <div class='flex-element'>
+                <div id='config-location'>{configSignals.location}</div>
+            </div>
+            <div class='flex-element'>
+                <div id='config-timezone'>{configSignals.timezone}</div>
+            </div>
+            <div class='flex-element'>
+                <div id='config-latitude'>{configSignals.latitude}</div>
+            </div>
+            <div class='flex-element'>
+                <div id='config-longitude'>{configSignals.longitude}</div>
+            </div>
+        </div>
+  </div>;
+};
