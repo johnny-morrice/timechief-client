5c6280f5c57bb6db30a1a01ee0851c069609fa7e
John Morrice <john.morrice.developer@gmail.com>
2022-04-24 04:06:12 +0100

    clock looking nice

--- src/renderer/homePage.jsx
+++ src/renderer/homePage.jsx
@@ -67,8 +67,8 @@ export const HomePage = () => {
         }}
         >
         <div class="column-flex">
-            <div class='flex-element' id='home-location'>{homePageSignals.location}</div>
             <div class='flex-element' id='time'>{homePageSignals.myTime}</div>
+            <div class='flex-element' id='home-location'>{homePageSignals.location}</div>
             <div class='flex-element' id='date'>{homePageSignals.myDate}</div>
 
             <div class='section-name flex-element'>Temperature</div>
@@ -76,7 +76,7 @@ export const HomePage = () => {
                 <div class='flex-element' id='temperature'>{homePageSignals.temp}</div>
                 <div class='flex-element' id='feels-like-temperature'>{homePageSignals.feelsLikeTemp}</div>
             </div>
-            <div class="column-flex flex-element">
+            <div class="column-flex">
                 <div class='section-name flex-element'>Weather today</div>
                 <Index each={homePageSignals.weatherDescriptions()}>{(desc, i) => {
                     console.log(`home page description: ${desc()}`);
