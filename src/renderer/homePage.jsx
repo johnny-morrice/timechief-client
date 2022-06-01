9716df9086b397a6ce812c96810211ea17c47890
John Morrice <john.morrice.developer@gmail.com>
2022-06-01 19:22:43 +0100

    dont show status if everything is OK

--- src/renderer/homePage.jsx
+++ src/renderer/homePage.jsx
@@ -164,17 +164,17 @@ export const HomePage = () => {
         </div>
         <div class="column-flex flex-element">
           <div class={"row-flex home-health-icon-bar " + errorStyleClass(homePageSignals.isAPIError())}>
-            <Show when={!homePageSignals.isAPIError()}>
+            {/* <Show when={!homePageSignals.isAPIError()}>
               <div class='flex-element home-health-icon'>
                 <i class='fa-solid fa-heart'></i>
               </div>
-            </Show>
+            </Show> */}
             <Show when={homePageSignals.isAPIError()}>
               <div class='flex-element'>
                 <i class='fa-solid fa-heart-crack'></i>
               </div>
-            </Show>
             <div class='flex-element'>{homePageSignals.lastRefreshText}</div>
+            </Show>
           </div>
         </div>
       </div>
