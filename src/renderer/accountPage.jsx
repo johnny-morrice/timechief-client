9f3a244c19beafd14159e87a3c64be1df1f655fe
John Morrice <john.morrice.developer@gmail.com>
2022-06-14 19:18:54 +0100

    handle pairing

--- src/renderer/accountPage.jsx
+++ src/renderer/accountPage.jsx
@@ -1,5 +1,5 @@
 import { createSignal } from 'solid-js';
-import { addClockDataCallback } from './ipc';
+import { addClockDataCallback, sendPairingCreateRequest, sendPairingGetRequest, sendPairingCompleteRequest, addPairingGetCallback, addPairingCompleteCallback, addPairingCreateCallback } from './ipc';
 
 class AccountPageSignals {
   constructor() {
@@ -18,9 +18,34 @@ function updateAccountPageSignals(signals, data) {
 var initialised = false;
 let accountSignals = new AccountPageSignals();
 export const AccountPage = () => {
-  
+  var pairingGetInterval = null;
   if (!initialised) {
     addClockDataCallback((data) => updateAccountPageSignals(accountSignals, data));
+    addPairingCreateCallback((data) => {
+        accountSignals.setPairingCode(data["PairingCode"]);
+        pairingGetInterval = setInterval(() => {
+            let pairingCode = accountSignals.pairingCode();
+            if (hasPairingCode(pairingCode)) {
+                sendPairingGetRequest(accountSignals.pairingCode());
+            }
+        }, 300);
+    });
+    addPairingCompleteCallback((data) => {
+        accountSignals.setPairingCode("");
+        if (pairingGetInterval != null) {
+            clearInterval(pairingGetInterval);
+        }
+    });
+    addPairingGetCallback((data) => {
+        if (data["Status"] == "linked") {
+            let pairingCode = accountSignals.pairingCode();
+            if (hasPairingCode(pairingCode)) {
+                sendPairingCompleteRequest();
+            } else {
+                console.log("cannot complete pairing, lost pairing code");
+            }
+        }
+    });
     initialised = true;
   }
 
@@ -32,6 +57,10 @@ export const AccountPage = () => {
       return pairingCode.length > 0;
   }
 
+  function onClickLinkAccountButton() {
+    sendPairingCreateRequest();
+  }
+
   return <div id="account-screen">
         <Show when={hasPrincipalSerial(accountSignals.principalSerial())}>
         <div class="column-flex">
@@ -40,29 +69,37 @@ export const AccountPage = () => {
                 <div class="flex-element data-name">Principal serial</div>
                 <div class="flex-element data-value">{accountSignals.principalSerial}</div>
             </div>
-            <div class="flex-element">
-                <button>Link your account <i class="fa-solid fa-user-plus"></i></button>
-            </div>
+            <Show when={!hasPairingCode(accountSignals.pairingCode())}>
+                <div class='row-flex flex-element'>
+                    <div class="flex-element">
+                        <button onClick={onClickLinkAccountButton}>Link your account <i class="fa-solid fa-user-plus"></i></button>
+                    </div>
+                </div>
+            </Show>
         </div>
         </Show>
         <Show when={!hasPrincipalSerial(accountSignals.principalSerial())}>
         <div class="column-flex">
             <div class="flex-element section-name underline">Link your account</div>
-            <div class='row-flex flex-element'>
-                <div class="flex-element">
-                    <button>Link your account <i class="fa-solid fa-user-plus"></i></button>
+            <Show when={!hasPairingCode(accountSignals.pairingCode())}>
+                <div class='row-flex flex-element'>
+                    <div class="flex-element">
+                        <button onClick={onClickLinkAccountButton}>Link your account <i class="fa-solid fa-user-plus"></i></button>
+                    </div>
                 </div>
-            </div>
+            </Show>
         </div>
         </Show>
         <Show when={hasPairingCode(accountSignals.pairingCode())}>
-            <div class='row-flex flex-element'>
-                <div class="flex-element data-name">Go to</div>
-                <div class="flex-element data-value">(The URL)</div>
-            </div>
-            <div class='row-flex flex-element'>
-                <div class="flex-element data-name">Enter your pairing code</div>
-                <div class="flex-element data-value">{accountSignals.principalSerial}</div>
+            <div class="column-flex">
+                <div class='row-flex flex-element'>
+                    <div class="flex-element data-name">Go to</div>
+                    <div class="flex-element data-value">(The URL)</div>
+                </div>
+                <div class='row-flex flex-element'>
+                    <div class="flex-element data-name">Enter your pairing code</div>
+                    <div class="flex-element data-value">{accountSignals.principalSerial}</div>
+                </div>
             </div>
         </Show>
   </div>;
