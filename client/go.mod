0036c53882a9d8cbfb7c7327b26cd39ca3c7efff
John Morrice <john.morrice.developer@gmail.com>
2023-03-09 20:44:00 +0000

    add VersionClient

--- client/go.mod
+++ client/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-resty/resty/v2 v2.7.0
+	github.com/google/go-cmp v0.5.9
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/sarulabs/di/v2 v2.4.2
@@ -13,7 +14,6 @@ require (
 )
 
 require (
-	github.com/google/go-cmp v0.5.9 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
--- client/publicclient/client.go
+++ client/publicclient/client.go
@@ -1,7 +1,41 @@
 package publicclient
 
-import "github.com/johnny-morrice/timechief-client/client/client"
+import (
+	"github.com/johnny-morrice/timechief-client/client/client"
+	"github.com/johnny-morrice/timechief-client/client/framework"
+	"github.com/johnny-morrice/timechief-client/client/log"
+	"github.com/sarulabs/di/v2"
+)
 
 type Client struct {
-	*client.Client
+	*client.RestClient
+}
+
+func RegisterClient(builder *di.Builder) error {
+	return builder.Add(
+		di.Def{
+			Name:  framework.PublicClient,
+			Scope: di.App,
+			Build: func(app di.Container) (interface{}, error) {
+				cfg := GetPublicClientConfig(app)
+				logger := log.GetLogger(app)
+				restClient := &client.RestClient{
+					RawRestClient: &client.RawRestClient{
+						Config:  cfg.ClientConfig,
+						Options: cfg.Options(),
+						Logger:  logger,
+					},
+					Logger: logger,
+				}
+				client := &Client{
+					RestClient: restClient,
+				}
+				return client, nil
+			},
+		},
+	)
+}
+
+func GetClient(app di.Container) *Client {
+	return app.Get(framework.PublicClient).(*Client)
 }
