ace36ddafd0c86279b89685e842790ac2a0460d7
John Morrice <john.morrice.developer@gmail.com>
2023-03-21 02:48:55 +0000

    standardise bundle naming

--- launcher/cmd/target/run.go
+++ launcher/cmd/target/run.go
@@ -8,9 +8,12 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const tarDirectory = "timechief-client-bundle"
+const execName = "timechief-client"
+
 func Run(ctx *cli.Context) error {
 	targetRoot := ctx.String("target-root")
-	clientExecutable := filepath.Join(targetRoot, "timechief-client")
+	clientExecutable := filepath.Join(targetRoot, tarDirectory, execName)
 	err := exec.Command(clientExecutable).Run()
 	if err != nil {
 		return fmt.Errorf("failed to run client at %s: %w", clientExecutable, err)
