61ce572956a043cb0de8a8a7af34e3928f4ecbeb
John Morrice <john.morrice.developer@gmail.com>
2023-03-21 21:07:51 +0000

    send target root to client

--- launcher/cmd/target/run.go
+++ launcher/cmd/target/run.go
@@ -15,7 +15,7 @@ const execName = "timechief-client"
 func Run(ctx *cli.Context) error {
 	targetRoot := ctx.String("target-root")
 	clientExecutable := filepath.Join(targetRoot, tarDirectory, execName)
-	out, err := exec.Command(clientExecutable).CombinedOutput()
+	out, err := exec.Command(clientExecutable, targetRoot).CombinedOutput()
 	log.Println("client output: ", string(out))
 	if err != nil {
 		return fmt.Errorf("failed to run client at %s: %w", clientExecutable, err)
