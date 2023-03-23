3efddb7d6bb39d1eeb9ba3b857e3bbd42cbc0f88
John Morrice <john.morrice.developer@gmail.com>
2023-03-23 02:31:04 +0000

    tar file is local even if it has a colon

--- launcher/store/launchtarget.go
+++ launcher/store/launchtarget.go
@@ -125,7 +125,7 @@ func mkdirp(dirpath string) error {
 // extractTarball shells out to the tar utility to extract a tarball.
 func extractTarball(tarballPath string, destination string) error {
 	log.Printf("extracting tarball %s to %s", tarballPath, destination)
-	output, err := exec.Command("tar", "-xvf", tarballPath, "-C", destination).CombinedOutput()
+	output, err := exec.Command("tar", "--force-local", "-xvf", tarballPath, "-C", destination).CombinedOutput()
 	log.Printf("tar output: %s", output)
 	if err != nil {
 		return fmt.Errorf("failed to extract tarball: %w", err)
