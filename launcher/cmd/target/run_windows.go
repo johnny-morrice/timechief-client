e62e2602b666b5d817c7c56a09df9a6f469330a1
John Morrice <john.morrice.developer@gmail.com>
2023-03-23 03:06:42 +0000

    fix windows name errors

--- launcher/cmd/target/run_windows.go
+++ launcher/cmd/target/run_windows.go
@@ -18,11 +18,11 @@ func Run(ctx *cli.Context) error {
 	targetRoot := ctx.String("target-root")
 	logFile := ctx.String("log-file")
 	targetBundle := filepath.Join(targetRoot, tarDirectory)
-	script := filepath.Join(targetBundle, execName)
+	script := filepath.Join(targetBundle, scriptName)
 	out, err := exec.Command("powershell", script, targetBundle, logFile).CombinedOutput()
 	log.Println("client output: ", string(out))
 	if err != nil {
-		return fmt.Errorf("failed to run client at %s: %w", clientExecutable, err)
+		return fmt.Errorf("failed to run client at %s: %w", script, err)
 	}
 	return nil
 }
