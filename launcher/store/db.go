77f97fba482a38ea9de1bffe0f917db188d5d2d7
John Morrice <john.morrice.developer@gmail.com>
2023-03-23 02:17:25 +0000

    fix move func

--- launcher/store/db.go
+++ launcher/store/db.go
@@ -1,22 +1,11 @@
 package store
 
 import (
-	"path/filepath"
-
-	"github.com/urfave/cli/v2"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func getGormConfig() *gorm.Config {
 	return &gorm.Config{}
-}
-
-// GetDBConnection gets a GORM database connection for a SQLite3 database.
-func GetDBConnection(ctx *cli.Context) (*gorm.DB, error) {
-	installRoot := ctx.String("install-root")
-	dbPath := filepath.Join(installRoot, "timechief-launcher.db")
-	return gorm.Open(sqlite.Open(dbPath), getGormConfig())
 }
 
 func AutoMigrate(db *gorm.DB) error {
