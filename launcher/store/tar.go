ccc7115669adff1a199c3c09566b1df4e6df52a3
John Morrice <john.morrice.developer@gmail.com>
2023-03-23 02:43:30 +0000

    fix windows cope

--- launcher/store/tar.go
+++ launcher/store/tar.go
@@ -6,16 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-
-	"github.com/johnny-morrice/timechief-client/launcher/windows"
 )
 
 // extractTarball shells out to the tar utility to extract a tarball.
 func extractTarball(tarballPath string, destination string) error {
 	log.Printf("extracting tarball %s to %s", tarballPath, destination)
-	tarballPath = windows.PathCope(tarballPath)
-	destination = windows.PathCope(destination)
-	log.Printf("rewrote windows paths to %s %s", tarballPath, destination)
 	output, err := exec.Command("tar", "--force-local", "-xvf", tarballPath, "-C", destination).CombinedOutput()
 	log.Printf("tar output: %s", output)
 	if err != nil {
--- launcher/store/tar_windows.go
+++ launcher/store/tar_windows.go
@@ -1,3 +1,25 @@
 //go:build windows
 
 package store
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/johnny-morrice/timechief-client/launcher/windows"
+)
+
+// extractTarball shells out to the tar utility to extract a tarball.
+func extractTarball(tarballPath string, destination string) error {
+	log.Printf("extracting tarball %s to %s", tarballPath, destination)
+	tarballPath = windows.PathCope(tarballPath)
+	destination = windows.PathCope(destination)
+	log.Printf("rewrote windows paths to %s %s", tarballPath, destination)
+	output, err := exec.Command("tar", "--force-local", "-xvf", tarballPath, "-C", destination).CombinedOutput()
+	log.Printf("tar output: %s", output)
+	if err != nil {
+		return fmt.Errorf("failed to extract tarball: %w", err)
+	}
+	return nil
+}
