63b4fdbd2dd57314947ac6bb76b2de76492761d6
John Morrice <john.morrice.developer@gmail.com>
2023-03-26 19:56:39 +0000

    install timechief in the os image

--- image/stage2-kiosk/00-kiosk/files/.profile
+++ image/stage2-kiosk/00-kiosk/files/.profile
@@ -26,8 +26,13 @@ alias l='ls -CF'
 alias s='sudo'
 alias sus='sudo -s'
 
-# first-run script
-[[ -f ./firstrun.sh ]] && ./firstrun.sh
+# If the file "not-first-run" does not exist, then run ./firstrun.sh.
+# This is a hack to run the firstrun.sh script only once.
+# The firstrun.sh script will create the file "not-first-run" and
+# then this script will not run it again.
+if [ ! -f ./not-first-run ]; then
+    ./firstrun.sh
+fi
 
 # silent startx on video console
 
--- image/stage2-kiosk/00-kiosk/files/kiosk.sh
+++ image/stage2-kiosk/00-kiosk/files/kiosk.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# read URL from easily accessible location
-URL=$(head -n 1 /boot/kiosk.url)
-
 # never blank the screen
 xset s off -dpms
 
 # rotate to portrait mounted TV
-xrandr --output HDMI-1 --rotate left
+# xrandr --output HDMI-1 --rotate left
 
 # show a splash before browser kicks in
 feh --bg-scale splash.png
 
 # start the cec-client & browser
-(cec-client | cec2kbd) & browser --fullscreen "${URL:='https://deltazero.cz'}"
+/var/opt/timechief-launcher run-client
