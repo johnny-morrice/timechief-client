39fa61cd8ba31efb87648e13b091424d24177baa
John Morrice <john.morrice.developer@gmail.com>
2023-03-30 01:05:00 +0000

    autologin the kiosk

--- image/stage2-kiosk/00-kiosk/01-run.sh
+++ image/stage2-kiosk/00-kiosk/01-run.sh
@@ -14,6 +14,25 @@ EOF
 
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
+HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
+install -m 755 -o 1000 -g 1000 files/kiosk.sh "${HOME}/"
+install -m 755 -o 1000 -g 1000 files/firstrun.sh "${HOME}/"
+install -m 644 -o 1000 -g 1000 files/.profile "${HOME}/"
+install -m 644 -o 1000 -g 1000 files/.xinitrc "${HOME}/"
+install -m 644 -o 1000 -g 1000 files/.hushlogin "${HOME}/"
+install -m 755 -o 1000 -g 1000 files/splash.png "${HOME}/"
+install -m 755 -o 1000 -g 1000 -d "${HOME}/bin/"
 
 cp -r "${TIMECHIEF_ROOT}" "${ROOTFS_DIR}/opt"
 find "${ROOTFS_DIR}/opt/timechief-launcher" | xargs chown 1000:1000
+
+# Autologin
+
+on_chroot << EOF
+    systemctl --quiet set-default multi-user.target
+    cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << CATEND
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $FIRST_USER_NAME --noclear %I \$TERM
+CATEND
+EOF
