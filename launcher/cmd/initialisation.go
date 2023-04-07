5bbd1847ca8ae934acffa3f3695e10d9cb3e81c2
John Morrice <john.morrice.developer@gmail.com>
2023-04-07 17:55:29 +0000

    set device credentials in db

--- launcher/cmd/initialisation.go
+++ launcher/cmd/initialisation.go
@@ -11,7 +11,7 @@ import (
 
 // Read the given configKeys from the cli.Context and return a store.Config instance.
 func cfgFlags(ctx *cli.Context) store.Config {
-	configKeys := []string{"install-root", "api-base-url", "product", "stream"}
+	configKeys := []string{"install-root", "api-base-url", "product", "stream", "device-credentials"}
 	cfg := store.Config{
 		Config: make(map[string]string),
 	}
