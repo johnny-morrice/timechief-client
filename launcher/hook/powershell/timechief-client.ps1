d666067c1585b8416d692a4782c53a3a8cbf70ce
John Morrice <john.morrice.developer@gmail.com>
2023-04-16 16:51:08 +0000

    pass client version env var through hook

--- launcher/hook/powershell/timechief-client.ps1
+++ launcher/hook/powershell/timechief-client.ps1
@@ -1,12 +1,15 @@
 $ErrorActionPreference = "Stop"
 $WORK_DIR = $args[0]
 $LOG_FILE = $args[1]
+$CLIENT_VERSION = $args[2]
 
-if (-not($WORK_DIR) -or -not($LOG_FILE)) {
-  Write-Host "Usage: timechief-client.ps1 <work-dir> <log-file>"
+if ((-not($WORK_DIR) -or -not($LOG_FILE)) -or -not($CLIENT_VERSION)) {
+  Write-Host "Usage: timechief-client.ps1 <work-dir> <log-file> <client-version>"
   exit 1
 }
 
 Set-Location $WORK_DIR
 Set-Location win-unpacked
+# Set client version environment variable
+$env:clientVersion = $CLIENT_VERSION
 ./timechief.exe .
