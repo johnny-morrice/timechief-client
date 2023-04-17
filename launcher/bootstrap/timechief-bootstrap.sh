ff8686271d1e59ad767847e320e5d266eb5e233f
John Morrice <john.morrice.developer@gmail.com>
2023-04-17 14:53:02 +0000

    bootstrap file is in the launcher

---
+++ launcher/bootstrap/timechief-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# never blank the screen
+xset s off -dpms
+
+# rotate to portrait mounted TV
+# xrandr --output HDMI-1 --rotate left
+
+# show a splash the app boots
+feh --bg-scale /opt/timechief-launcher/image/splash.png
+
+# start timechief
+/opt/timechief-launcher/bin/timechief-launcher run-client >> /opt/timechief-launcher/timechief-launcher.log 2>&1
