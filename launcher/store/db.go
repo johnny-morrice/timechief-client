3cc81d457cc058e4b9fadb456a359b43217863b2
John Morrice <john.morrice.developer@gmail.com>
2023-05-17 19:22:47 +0000

    set up gorm logger

--- launcher/store/db.go
+++ launcher/store/db.go
@@ -1,11 +1,26 @@
 package store
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func getGormConfig() *gorm.Config {
-	return &gorm.Config{}
+	logger := logger.New(
+		log.New(os.Stderr, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      false,         // Don't include params in the SQL log
+			Colorful:                  false,         // Disable color
+		},
+	)
+	return &gorm.Config{Logger: logger}
 }
 
 func AutoMigrate(db *gorm.DB) error {
