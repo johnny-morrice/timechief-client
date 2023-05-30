698173934b9abe50dec7b183b384dcece0ba5ee7
John Morrice <john.morrice.developer@gmail.com>
2023-05-30 23:39:54 +0100

    use constant

--- launcher/system/system.go
+++ launcher/system/system.go
@@ -325,7 +325,7 @@ func (sys System) LoadNetworkStatus() error {
 	if err != nil {
 		return fmt.Errorf("failed to get network status: %w", err)
 	}
-	if status.Mode == "Managed" && status.SSID != "" {
+	if status.Mode == InfraMode && status.SSID != "" {
 		if status.State == "up" {
 			log.Printf("connected to %s", status.SSID)
 			err = sys.WifiNetworkStore.MarkConnectionSuccess(status.SSID)
