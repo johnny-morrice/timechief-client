9579386c37b56a47b7951a5535cfb901ee34ff4b
John Morrice <john.morrice.developer@gmail.com>
2023-06-02 03:26:57 +0100

    fix crashes on null

--- electron/src/renderer/webSetupPage.jsx
+++ electron/src/renderer/webSetupPage.jsx
@@ -40,7 +40,7 @@ function updateWebSetupPageSignals(signals, data) {
             signals.setActiveSSID(activeSSID);
             signals.setWifiError(wifiError);
         
-            if (hotspotSSID.length > 0 && hotspotKey.length > 0) {
+            if (hotspotID && hotspotSSID.length > 0 && hotspotKey.length > 0) {
                 signals.setHotspotSSID(hotspotSSID);
                 signals.setHotspotKey(hotspotKey);
             }
