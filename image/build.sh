70014ea67fd9cd2744cda4f3feba22ee626266e1
John Morrice <john.morrice.developer@gmail.com>
2023-06-04 20:14:06 +0100

    put some guardrails into the build script

--- image/build.sh
+++ image/build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+CHECK_FILE=/opt/timechief-launcher/bin/timechief-launcher
+
+# If CHECK_FILE does not exist, exit with error.
+if [ ! -f "$CHECK_FILE" ]; then
+    echo "File $CHECK_FILE does not exist. Exiting."
+    exit 1
+fi
+
 # shellcheck disable=SC2164
 (cd ./pi-gen; ./build.sh)
