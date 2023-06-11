000c3e9d014f565cfe83b5ae850803943bab5fc8
John Morrice <john.morrice.developer@gmail.com>
2023-06-11 18:44:36 +0100

    update web content

--- launcher/fileserver/static/assets/index.36108498.js
+++ launcher/fileserver/static/assets/index.36108498.js
@@ -735,8 +735,29 @@ function cleanChildren(parent, current, marker, replacement) {
 
 const isServer = false;
 
+async function getDeviceData() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+  const url = `/api/data/device`;
+  return await fetch(url, options).then(response => response.json());
+}
+
 const _tmpl$ = /*#__PURE__*/template(`<div><div id="app-root"><nav class="site-nav-bar"><div class="main-links"><div class="site-name-home">Timechief</div><div class="bar-devices-wrapper"><div class="bar-device-setup">DEVICE SETUP</div></div></div><div class="bar-account-wrapper"><div class="status-indicator"><i class="fa-solid fa-heart"></i></div></div></nav><div class="root-wrapper"><div class="main-content"><h1>It works!</h1><p>Font awesome loaded indicator below</p><i class="fa-solid fa-thumbs-up"></i></div></div><footer class="footer">Footer content</footer></div></div>`, 32);
 const Home = () => {
+  // Start a poll for device data
+  // This is going to function as a heartbeat but all the crucial information we need is also there.
+  function pollDeviceData() {
+    getDeviceData().then(data => {
+      console.log(data);
+    });
+  }
+
+  // Poll every 5 seconds
+  setInterval(pollDeviceData, 5000);
   return _tmpl$.cloneNode(true);
 };
 
