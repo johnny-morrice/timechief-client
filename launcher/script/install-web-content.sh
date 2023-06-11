72348fdfd123b5ac0052340ce8f4e5ef8cef8cce
John Morrice <john.morrice.developer@gmail.com>
2023-06-11 18:35:11 +0100

    implement web files copy script

--- launcher/script/install-web-content.sh
+++ launcher/script/install-web-content.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Delete files in fileserver/static
+rm -rf ./fileserver/static/*
+
+# Copy files from ../launcher-web/dist to fileserver/static
+cp -r ../launcher-web/dist/* ./fileserver/static/
+
+# Check that files were copied, by checking for existence of index.html
+if [ -f ./fileserver/static/index.html ]; then
+    echo "Web content copied successfully"
+else
+    echo "Web content copy failed"
+    exit 1
+fi
