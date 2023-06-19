0e9bb19dbd8273a967a9e7ba33455eb33c7ea1eb
John Morrice <john.morrice@github.com>
2023-06-19 21:53:33 +0100

    add gitignore

---
+++ rtcutil/pyrtc/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyrtc',
+      version='0.0.1',
+      description='Python RTC application for timechief',
+      url='https://github.com/johnny-morrice/timechief-client/',
+      author='Johnny Morrice',
+      author_email='john.morrice.developer@gmail.com',
+      license='All Rights Reserved',
+      packages=['pyrtc'],
+    #   install_requires=[
+        #   'markdown',
+    #   ],
+      zip_safe=False)
