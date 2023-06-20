3d5f46c1fb004600494a702d2063fc3816749d2c
John Morrice <john.morrice@github.com>
2023-06-20 20:39:21 +0100

    fix security hole

--- rtcutil/pyrtc/pyrtc/pyrtc.py
+++ rtcutil/pyrtc/pyrtc/pyrtc.py
@@ -9,7 +9,6 @@ def main():
     # Add the positional arguments
     parser.add_argument('action', choices=['timeset', 'timeget', 'timesync'], help='Action to perform (timeset or timeget)')
     parser.add_argument('--type', choices=['rv3028'], help='RTC type (rv3028)')
-    parser.add_argument('--sync-script', default='/opt/timechief-launcher/bin/secure/set-system-time.sh', help='Path to time sync script')
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -48,7 +47,8 @@ def timesync(args):
     rtc = get_rtc()
     rtc_time = rtc.get_time_and_date()
     date_string = f"{rtc_time.year:04d}-{rtc_time.month:02d}-{rtc_time.day:02d}T{rtc_time.hour:02d}:{rtc_time.minute:02d}:{rtc_time.second:02d}Z"
-    subprocess.run([args.sync_script, date_string])
+    sync_script = "/opt/timechief-launcher/bin/secure/set-system-time.sh"
+    subprocess.run([sync_script, date_string])
 
 def get_rtc():
     # Create RV3028 instance
