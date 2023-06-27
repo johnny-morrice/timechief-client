547c4bfc6f28199ca8985f18165a9be616e07e75
John Morrice <john.morrice.developer@gmail.com>
2023-06-27 16:07:12 +0100

    log the gpu status

--- electron/src/main/main.js
+++ electron/src/main/main.js
@@ -107,7 +107,7 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
+  logger.info("GPU status: ", app.getGPUFeatureStatus());
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
