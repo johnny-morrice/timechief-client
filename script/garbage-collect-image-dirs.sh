c8ac20bb25159e2c23007b9bed89ea1b432e4108
John Morrice <john.morrice.developer@gmail.com>
2023-07-01 00:51:43 +0100

    yell at user to check the mounts

--- script/garbage-collect-image-dirs.sh
+++ script/garbage-collect-image-dirs.sh
@@ -2,6 +2,20 @@
 set -e
 set -x
 
+# Parameters
+#
+# FORCE
+
+FORCE="$1"
+
+# If FORCE is not "--force", then we will not delete the build directory
+if [ "$FORCE" != "--force" ] ; then
+  echo "apply --force to delete build directories"
+  echo "    are you sure you unmounted all the mounts?"
+  exit 1
+fi
+
+
 TIDY_NOTE="$HOME/scratch/tidy-images.txt"
 
 # The file TIDY_NOTE contains a list of directories to delete.
