ff3503c05048493787c1f6a4e85270814f4a8cce
John Morrice <john.morrice.developer@gmail.com>
2024-01-07 02:19:30 +0000

    remove debug from generated code

--- launcher/launcher/client/timechief/v2/gen.go
+++ launcher/launcher/client/timechief/v2/gen.go
@@ -12,7 +12,6 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"log"
 
 	"github.com/oapi-codegen/runtime"
 )
@@ -2663,10 +2662,6 @@ func NewListLatestVersionsRequest(server string, product string, stream string) 
 	if err != nil {
 		return nil, err
 	}
-
-	// TODO delete these debugs
-	log.Println("operationPath: ", operationPath)
-	log.Println("query URL: ", queryURL.String())
 
 	req, err := http.NewRequest("GET", queryURL.String(), nil)
 	if err != nil {
