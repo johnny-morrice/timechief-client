fb763ef96cdee3ac013beb33af65a38c2ffa4c76
John Morrice <john.morrice.developer@gmail.com>
2024-01-11 04:38:03 +0000

    initialise auth0-audience flag

--- launcher/cmd/initialisation.go
+++ launcher/cmd/initialisation.go
@@ -13,7 +13,7 @@ import (
 
 // Read the given configKeys from the cli.Context and return a store.Config instance.
 func cfgFlags(ctx *cli.Context) store.Config {
-	configKeys := []string{"install-root", "api-base-url", "product", "stream", "auth0-client-id"}
+	configKeys := []string{"install-root", "api-base-url", "product", "stream", "auth0-client-id", "auth0-audience"}
 	cfg := store.Config{
 		Config: make(map[string]string),
 	}
