5327f23d8de201b92ffd8bab82b10d3b55412f3b
John Morrice <john.morrice.developer@gmail.com>
2024-01-20 15:17:13 +0000

    logout button working

--- electron/src/renderer/nostro/deviceControl.jsx
+++ electron/src/renderer/nostro/deviceControl.jsx
@@ -1,6 +1,6 @@
 import { createSignal, onCleanup } from 'solid-js';
 import { callbackName } from "./callback";
-import { addDataCallback, addDeviceStatusCallback, removeDataCallback, removeDeviceStatusCallback, sendReboot, sendLoggedIn, sendSetupBegin } from './ipc';
+import { addDataCallback, addDeviceStatusCallback, removeDataCallback, removeDeviceStatusCallback, sendReboot, sendLoggedIn, sendSetupBegin, sendLogOut } from './ipc';
 import { buttonGlitchStyle, runButtonGlitch } from './textGlitch';
 import { Loading } from './loading';
 import { labelMaker, textMaker } from './label';
