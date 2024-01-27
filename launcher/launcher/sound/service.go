2bf0880447bc989b7f568e9b7e6629c7294be443
John Morrice <john.morrice.developer@gmail.com>
2024-01-27 18:03:19 +0000

    sound service checks for mute status

--- launcher/launcher/sound/service.go
+++ launcher/launcher/sound/service.go
@@ -1,16 +1,81 @@
 package sound
 
-import "github.com/johnny-morrice/timechief-client/launcher/launcher/client/daemonclient"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/johnny-morrice/timechief-client/launcher/launcher/client/daemonclient"
+	"gorm.io/gorm"
+)
 
 type Service struct {
 	dc daemonclient.DaemonClient
+	kv KeyValueStore
 }
 
-func NewSoundService(dc daemonclient.DaemonClient) Service {
-	return Service{dc: dc}
+type KeyValueStore interface {
+	Get(key string) (string, error)
+}
+
+func NewSoundService(dc daemonclient.DaemonClient, kv KeyValueStore) (Service, error) {
+	if kv == nil {
+		return Service{}, fmt.Errorf("kv is nil")
+	}
+	return Service{dc: dc, kv: kv}, nil
+}
+
+func (svc Service) isMuted() (bool, error) {
+	mute, err := svc.kv.Get("mute")
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, err
+	}
+	return mute == "true", nil
+}
+
+func (svc Service) isInUnmuteRange() (bool, error) {
+	timeNow := time.Now()
+	unmuteRange, err := svc.kv.Get("unmute-range")
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, err
+	}
+	if unmuteRange == "" {
+		return false, nil
+	}
+	rangeTimesText := strings.Split(unmuteRange, ",")
+	if len(rangeTimesText) != 2 {
+		return false, fmt.Errorf("unmute-range is invalid")
+	}
+	rangeTimes := make([]time.Time, 2)
+	for i, rangeTimeText := range rangeTimesText {
+		rangeTime, err := time.Parse(time.RFC3339, rangeTimeText)
+		if err != nil {
+			return false, err
+		}
+		rangeTimes[i] = rangeTime
+	}
+	unmuteStartTime := rangeTimes[0]
+	unmuteEndTime := rangeTimes[1]
+	isInRange := timeNow.After(unmuteStartTime) && timeNow.Before(unmuteEndTime)
+	return isInRange, nil
 }
 
 func (svc Service) playSound(songName string) error {
+	muted, err := svc.isMuted()
+	if err != nil {
+		return err
+	}
+	if muted {
+		return nil
+	}
+	inRange, err := svc.isInUnmuteRange()
+	if err != nil {
+		return err
+	}
+	if !inRange {
+		return nil
+	}
 	req := daemonclient.PlaySoundRequest{
 		SongName: songName,
 	}
