0e547a6eb8018cec37826892a223e0d33bce5c89
John Morrice <john.morrice.developer@gmail.com>
2024-01-28 18:41:26 +0000

    set initial sound state in setup daemon

--- launcher/launcher/daemon/devicedata.go
+++ launcher/launcher/daemon/devicedata.go
@@ -69,19 +69,6 @@ func (dd DeviceData) doTick(ctx *cli.Context) error {
 	return nil
 }
 
-func (dd DeviceData) SetTestSoundSettings() error {
-	log.Println("setting test sound settings")
-	err := dd.keyValueStore.Set("mute", "false")
-	if err != nil {
-		return err
-	}
-	err = dd.keyValueStore.Set("unmute-range", "9-21")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 var DeviceDataErrorState = "device-data-error"
 var CalendarErrorState = "calendar-error"
 
