9482d3e19d1767551c212f138a2bcc50d6d9d50e
John Morrice <john.morrice.developer@gmail.com>
2024-02-12 19:12:09 +0000

    set theme from daemon

---
+++ electron/src/main/themer.js
@@ -0,0 +1,49 @@
+import { getMainWindow } from "./window";
+
+class Themer {
+    constructor() {
+        this.lastThemeCssKey = null;
+    }
+
+    setThemeFromData(data) {
+        if (data && data.theme_css) {
+            this.setTheme(data.theme_css);
+        }
+    }
+
+    setDefaultTheme() {
+        this.setTheme(getDefaultThemeCSS());
+    }
+
+    setTheme(theme) {
+        if (this.lastThemeCssKey) {
+            getMainWindow().webContents.removeInsertedCSS(this.lastThemeCssKey);
+        }
+        getMainWindow().webContents.insertCSS(theme).then(key => {
+            this.lastThemeCssKey = key;
+        });
+    }
+
+}
+
+function getDefaultThemeCSS() {
+    return `body {
+    color: green;
+    background-color: black;
+    font-family: Titillium Web;
+}
+
+div.home-time {
+    font-family: Seven Segment;
+}
+
+div.ui-box {
+    background-color: black;
+}
+
+div.border {
+    border-radius: 0;
+    border-width: 1px;
+    border-color: green;
+}`;
+}
