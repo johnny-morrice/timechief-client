12972898045b670e67570cc6db64c674eb27e8b6
John Morrice <john.morrice.developer@gmail.com>
2024-03-01 01:16:45 +0000

    decorate pictures with URL

--- electron/src/main/mediadecorator.js
+++ electron/src/main/mediadecorator.js
@@ -7,13 +7,26 @@ class MediaDecorator {
         return `${this.mediaRoot}/video/${fileName}`;
     }
 
+    makePictureURL(fileName) {
+        return `${this.mediaRoot}/picture/${fileName}`;
+    }
+
     decorateData(data) {
-        if (data && data.media && data.media.video && data.media.video.videos) {
-            const myVideos = data.media.video.videos.map(video => {
-                video.url = this.makeVideoURL(video.filename);
-                return video;
-            });
-            data.media.video.videos = myVideos;
+        if (data && data.media) {
+            if (data.media.video && data.media.video.videos) {
+                const myVideos = data.media.video.videos.map(video => {
+                    video.url = this.makeVideoURL(video.filename);
+                    return video;
+                });
+                data.media.video.videos = myVideos;
+            }
+            if (data.media.picture && data.media.picture.pictures) {
+                const myPictures = data.media.picture.pictures.map(picture => {
+                    picture.url = this.makePictureURL(picture.filename);
+                    return picture;
+                });
+                data.media.picture.pictures = myPictures;
+            }
         }
         return data;
     }
