871eee9faf4b65b091946cce4b90d7e1ca554022
John Morrice <john.morrice.developer@gmail.com>
2024-08-28 23:30:15 +0100

    add logging to text glitch

--- electron/src/renderer/nostro/textGlitch.jsx
+++ electron/src/renderer/nostro/textGlitch.jsx
@@ -70,10 +70,14 @@ export const textTransitionResource = (value, getter, setter, transform) => {
     const [textBuffer] = createResource(getter, transform);
     const [intermediate, setIntermediate] = createSignal("");
     const applyHighlight = (text) => {
+        console.log("textTransitionResource applying highlight");
         return highlightSpansGlitch(text, textBuffer());
     };
     const [out] = createResource(intermediate, applyHighlight);
+    var count = 0
     function doSet(data) {
+        console.log("textTransitionResource doSet " + count);
+        count++;
         setter(data);
         textTransitionGlitch(textBuffer, intermediate, setIntermediate, 3);
     }
@@ -85,10 +89,14 @@ export const textTransitionSignal = (value) => {
     const [buffer, setBuffer] = createSignal("");
     const [intermediate, setIntermediate] = createSignal("");
     const applyHighlight = (text) => {
+        console.log("textTransitionSignal applying highlight");
         return highlightSpansGlitch(text, buffer());
     };
     const [out] = createResource(intermediate, applyHighlight);
+    var count = 0
     function doSet(data) {
+        console.log("textTransitionSignal doSet " + count);
+        count++;
         setBuffer(data);
         textTransitionGlitch(buffer, intermediate, setIntermediate, 3);
     }
@@ -103,6 +111,7 @@ export const textTransitionGlitch = (buffer, display, setter, n) => {
     
     if (more) {
         const interval = setInterval(() => {
+            console.log("textTransitionGlitch interval");
             const more = transitionBuffer(buffer, display, setter, n);
             if (!more) {
                 clearInterval(interval);
