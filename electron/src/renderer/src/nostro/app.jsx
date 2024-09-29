4cc4759e3a507418d9d62149a3cb8c8e0b7bfdc1
John Morrice <john.morrice.developer@gmail.com>
2024-09-29 20:45:45 +0100

    mv stuff to resources rather than public

--- electron/src/renderer/src/nostro/app.jsx
+++ electron/src/renderer/src/nostro/app.jsx
@@ -1,6 +1,5 @@
 import { render } from 'solid-js/web';
 import { onCleanup } from 'solid-js';
-import { hashIntegration, Router, Routes, Route } from "@solidjs/router";
 import { initializeIPC } from './ipc';
 import { WebSetupPage } from "./webSetup";
 import { HomePage } from "./homePage";
@@ -21,12 +20,9 @@ const App = () => {
     ipcIntervals.forEach(interval => clearInterval(interval));
   });
 
-  return <Routes>
-        <Route path="/home" element={<AppScreen element={<HomePage/>}/>}/>
-        <Route path="/" element={<AppScreen element={<HomePage/>}/>}/>
-      </Routes>
+  return <AppScreen element={<HomePage/>}/>
 };
 
 export function attachApp() {
-    render(() => <Router source={hashIntegration()}><App></App></Router>, document.getElementById('app'));
+    render(() => <App />, document.getElementById('app'));
 }
