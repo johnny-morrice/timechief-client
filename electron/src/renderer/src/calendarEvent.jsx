0886e2429d12e34f11efd6847d2eb3d7c692a18c
John Morrice <john.morrice.developer@gmail.com>
2024-11-10 12:27:08 +0000

    even longer event text

--- electron/src/renderer/src/calendarEvent.jsx
+++ electron/src/renderer/src/calendarEvent.jsx
@@ -111,7 +111,7 @@ export class CalendarEvent {
         const segmenter = new Intl.Segmenter("en", {granularity: 'grapheme'});
         const segItr = segmenter.segment(this.data.short_text);
         const segArr = Array.from(segItr, ({segment}) => segment);
-        const maxLen = 13;
+        const maxLen = 15;
         if (segArr.length <= maxLen) {
             return this.data.short_text;
         }
