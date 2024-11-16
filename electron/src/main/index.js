d11696662f2db854df1de94e6f657b7198eed98c
John Morrice <john.morrice.developer@gmail.com>
2024-11-16 12:09:04 +0000

    remove comment

--- electron/src/main/index.js
+++ electron/src/main/index.js
@@ -60,7 +60,7 @@ function handleIPCAPICall(sendChan, receiveChan, apiCall) {
       .catch(error => {
         logger.error(`error calling ${sendChan} API: ${error}`);
         if (error instanceof AggregateError) {
-          console.log(e.message); // "All Promises rejected"
+          console.log(e.message);
           console.log(e.name);
           console.log(e.errors);
         }
