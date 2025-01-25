5b221c6eda71d01dc0bf866ddb1de6db6e89a5d9
John Morrice <john.morrice.developer@gmail.com>
2025-01-25 17:28:13 +0000

    open dns and dhcp ports

--- launcher/launcher/daemon/firewall/firewall.go
+++ launcher/launcher/daemon/firewall/firewall.go
@@ -73,7 +73,7 @@ func (daemon *FirewallDaemon) Start(ctx context.Context) {
 }
 
 func (daemon *FirewallDaemon) doTick() error {
-	gracePorts := []string{"80", "443"}
+	gracePorts := []string{"80", "443", "53", "67"}
 	if daemon.isGraceIncludeSSH {
 		gracePorts = append(gracePorts, "22")
 	}
