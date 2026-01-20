4bac38d37e52501ff58472db2a8cf9099d51807f
John Morrice <john.morrice.developer@gmail.com>
2026-01-20 21:12:40 +0000

    Use my git branch for testin pigen fix

--- image/init.sh
+++ image/init.sh
@@ -2,12 +2,7 @@
 set -e
 
 ## clone pi-gen
-git clone --branch arm64 https://github.com/RPI-Distro/pi-gen.git
-
-## TODO fix build
-pushd pi-gen
-git checkout 05a772481a892663bc8974b72ae50aab684e92bb 
-popd
+git clone --depth 1 --branch fix/arm64-bookworm-keyring https://github.com/johnny-morrice/pi-gen.git
 
 ## disable export for stage2
 touch ./pi-gen/stage2/SKIP_IMAGES ./pi-gen/stage2/SKIP_NOOBS
