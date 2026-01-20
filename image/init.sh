93fcedb394b2d234c029a73a5b3304d27fa006cf
John Morrice <john.morrice.developer@gmail.com>
2026-01-20 00:20:25 +0000

    Fix build by rewinding before bug

--- image/init.sh
+++ image/init.sh
@@ -3,6 +3,11 @@
 ## clone pi-gen
 git clone --depth 1 --branch arm64 https://github.com/RPI-Distro/pi-gen.git
 
+## TODO fix build
+pushd pi-gen
+git checkout 05a772481a892663bc8974b72ae50aab684e92bb 
+popd
+
 ## disable export for stage2
 touch ./pi-gen/stage2/SKIP_IMAGES ./pi-gen/stage2/SKIP_NOOBS
 
