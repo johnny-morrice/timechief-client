3589fbbb35e9399c548fc6129b231ef95a373c7f
John Morrice <john.morrice.developer@gmail.com>
2026-01-20 21:33:53 +0000

    Use bookworm arm64 tag

--- image/init.sh
+++ image/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 ## clone pi-gen
-git clone --depth 1 --branch fix/arm64-bookworm-keyring https://github.com/johnny-morrice/pi-gen.git
+git clone --depth 1 --branch bookworm-arm64 https://github.com/RPi-Distro/pi-gen.git
 
 ## disable export for stage2
 touch ./pi-gen/stage2/SKIP_IMAGES ./pi-gen/stage2/SKIP_NOOBS
